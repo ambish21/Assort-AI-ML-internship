e55555a639baece9b5f9c017e0af713860a24185
Ambish21 <ambishyaqoob2005@outlook.com>
2026-07-23 14:43:55 +0500

    update day 01 code using case match

--- Week-01/Day-01/calculator.py
+++ Week-01/Day-01/calculator.py
@@ -1,12 +1,8 @@
-# Simple Calculator
-
 print("===== Simple Calculator =====")
 
-# Take input from user
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 
-# Ask user to choose an operation
 print("\nChoose an operation")
 print("+ for Addition")
 print("- for Subtraction")
@@ -15,25 +11,22 @@ print("/ for Division")
 
 operator = input("Enter operator (+, -, *, /): ")
 
-# Perform calculation
-if operator == "+":
-    result = num1 + num2
-    print("Result =", result)
+match operator:
 
-elif operator == "-":
-    result = num1 - num2
-    print("Result =", result)
+    case "+":
+        print("Result =", num1 + num2)
 
-elif operator == "*":
-    result = num1 * num2
-    print("Result =", result)
+    case "-":
+        print("Result =", num1 - num2)
 
-elif operator == "/":
-    if num2 != 0:
-        result = num1 / num2
-        print("Result =", result)
-    else:
-        print("Error! Division by zero is not allowed.")
+    case "*":
+        print("Result =", num1 * num2)
 
-else:
-    print("Invalid operator!")
+    case "/":
+        if num2 != 0:
+            print("Result =", num1 / num2)
+        else:
+            print("Error! Division by zero is not allowed.")
+
+    case _:
+        print("Invalid operator!")
