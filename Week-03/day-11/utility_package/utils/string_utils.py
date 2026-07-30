366abc84e12b9ebf609564507a0b8fcb85dc5793
Ambish21 <ambishyaqoob2005@outlook.com>
2026-07-30 16:19:58 +0500

    DAY -11 calculator is updated and main file is added

---
+++ Week-03/day-11/utility_package/utils/string_utils.py
@@ -0,0 +1,16 @@
+# string_utils.py
+
+def to_upper(text):
+    return text.upper()
+
+
+def to_lower(text):
+    return text.lower()
+
+
+def reverse(text):
+    return text[::-1]
+
+
+def count_characters(text):
+    return len(text)
