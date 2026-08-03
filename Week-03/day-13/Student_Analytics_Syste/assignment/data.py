25a3e9f3dd613308e911c98c2ecf94e30027f4cb
Ambish21 <ambishyaqoob2005@outlook.com>
2026-08-03 10:00:00 +0500

    assignment data code is added

---
+++ Week-03/day-13/Student_Analytics_Syste/assignment/data.py
@@ -0,0 +1,15 @@
+"""
+Day 13 Assignment
+Student Data
+"""
+
+students = [
+    {"id": 101, "name": "Ali", "marks": 85},
+    {"id": 102, "name": "Sara", "marks": 92},
+    {"id": 103, "name": "Ahmed", "marks": 67},
+    {"id": 104, "name": "Fatima", "marks": 78},
+    {"id": 105, "name": "Usman", "marks": 55},
+    {"id": 106, "name": "Ayesha", "marks": 95},
+    {"id": 107, "name": "Bilal", "marks": 88},
+    {"id": 108, "name": "Hina", "marks": 72}
+]
